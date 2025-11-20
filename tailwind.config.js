b2b516c84aa02ec3ebe460abba09eb7533d125b2
Victor Rivas <vrivas@parameta.co>
2025-11-19 21:42:59 -0300

    Restarted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
