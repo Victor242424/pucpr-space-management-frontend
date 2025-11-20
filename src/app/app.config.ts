d37fe8c533a3a640e8d0d25046d013c5efa282e3
Victor Rivas <vrivas@parameta.co>
2025-11-20 12:51:40 -0300

    Correción de interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideHttpClient, HTTP_INTERCEPTORS, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()), // ← Habilitar interceptors DI
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -14,7 +14,6 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.authService.getToken();
-
     if (token) {
       req = req.clone({
         setHeaders: {
