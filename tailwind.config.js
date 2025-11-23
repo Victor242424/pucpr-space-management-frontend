a86e04a605ead170f9859bc54fe984f682d3b2bf
Victor Rivas <vrivas@parameta.co>
2025-11-23 19:50:34 -0300

    Corregido error de tailwing no eliminando CSS no utilizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+      "./src/**/*.{html,ts}",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -18,7 +20,7 @@ module.exports = {
           700: '#0369a1',
           800: '#075985',
           900: '#0c4a6e',
-        },// Puedes agregar más colores personalizados aquí
+        },
         secondary: {
           50: '#f8fafc',
           100: '#f1f5f9',
